48848b35d38a5c7ada34d2c81a9a644722012845
Tarun Singh <tarun@Taruns-MacBook-Air.local>
2025-10-15 11:05:28 +0530

    meta pixel

--- apps/client-main/src/app/layout.tsx
+++ apps/client-main/src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script"; // Import Next.js Script
 import "./globals.css";
 import { Appbar } from "@/components/Appbar";
 import { Providers } from "./provider";
@@ -73,16 +74,44 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${appFont.className} min-h-screen `}>
+        {/* Facebook Pixel */}
+        <Script
+          id="facebook-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '${process.env.NEXT_PUBLIC_FB_PIXEL_ID}');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          <img 
+            height="1" 
+            width="1" 
+            style={{ display: 'none' }}
+            src={`https://www.facebook.com/tr?id=${process.env.NEXT_PUBLIC_FB_PIXEL_ID}&ev=PageView&noscript=1`}
+            alt=""
+          />
+        </noscript>
         <Providers>
           <Appbar/>
-              <NavbarStateControlProvider />
-                {children}
-                <Analytics />
-                <SpeedInsights />
-              <Footer />
+          <NavbarStateControlProvider />
+          {children}
+          <Analytics />
+          <SpeedInsights />
+          <Footer />
           <Toaster />
         </Providers>
       </body>
     </html>
   );
-};
+}
