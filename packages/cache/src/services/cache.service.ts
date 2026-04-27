565c2e58e9493840a7251f6f9915a99f7a102a52
Tarun Singh <singhtarun1128@gmail.com>
2026-04-27 15:39:05 +0530

    visited cache fix

--- packages/cache/src/services/cache.service.ts
+++ packages/cache/src/services/cache.service.ts
@@ -4,16 +4,11 @@ export const cacheServicesRedisClient = () => connectToRedis()!;
 
 export const customCacheJSONIncr = async ({key, path, value=1}: {key: string, path:string|number, value?:number} ) => {
     try{
-        const keyExists = await cacheServicesRedisClient().exists(key);
-        if( !keyExists ){
-            await cacheServicesRedisClient().json.set(key, "$", {[path]: value});
-            return;
-        }
         const result = await cacheServicesRedisClient().json.numincrby( key, `$['${path}']`, value );
         if( result[0] == null ){
             await cacheServicesRedisClient().json.set(key, `$['${path}']`, value);
         }
-    } catch(error) {
-        console.log("Error in customCacheJSONIncr:", error);
+    } catch {
+        await cacheServicesRedisClient().json.set(key, "$", {[path]: value}).catch(() => {});
     }
 }
